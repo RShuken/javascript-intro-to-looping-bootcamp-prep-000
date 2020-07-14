93c8aa959c6153324a4713b489b546c6ddcbed6d
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 20:58:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
 
 function forLoop (array) {
   for (let i = 0, i < 24; i++){
-    console.log(i);
+    if (i === 1) {
+      console.log(`I am 1 strange loop.`)
+    } else {
+      console.log(`I am ${i} strange loop.`)
+    }
   }
 }
